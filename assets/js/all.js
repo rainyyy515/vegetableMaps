e44c2ac7903cbb8d225799f650133d11b37173e7
yanwei <yanwei3623918@gmail.com>
2022-08-21 20:58:27 +0800

    Update 2022-08-21T12:58:22.963Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,47 +1,53 @@
+"use strict";
+
 $(function () {
   console.log('Hello Bootstrap5');
   $('.nav-link').click(function (e) {
     $(this).toggleClass('focus');
     $(this).parent().siblings().find('.nav-link').removeClass('focus');
-  });
-  //輪播
+  }); //輪播
+
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      prevEl: ".swiper-button-prev"
     },
     breakpoints: {
       768: {
         slidesPerView: 2,
-        spaceBetween: 24,
+        spaceBetween: 24
       },
       1024: {
         slidesPerView: 3,
-        spaceBetween: 24,
-      },
-    },
+        spaceBetween: 24
+      }
+    }
   });
-
-
-  let map,infoWindow;
+  var map, infoWindow;
 
   function initMap() {
     //導入maps
     map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 22.579741781811244, lng: 120.32260202023902 },
-      zoom: 13,
-    });
-    //標記的 Google 地圖添加
-    const uluru = {lat:22.579008191337223 ,lng:120.32207265939651};
-    const marker = new google.maps.Marker({
+      center: {
+        lat: 22.579741781811244,
+        lng: 120.32260202023902
+      },
+      zoom: 13
+    }); //標記的 Google 地圖添加
+
+    var uluru = {
+      lat: 22.579008191337223,
+      lng: 120.32207265939651
+    };
+    var marker = new google.maps.Marker({
       position: uluru,
-      map: map,
+      map: map
     });
   }
-  
-  window.initMap = initMap;
 
+  window.initMap = initMap;
 });
+//# sourceMappingURL=all.js.map
